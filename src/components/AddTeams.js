357805a861f46432f164b46e915601f3f870dc92
Simeon Trenev <65025840+SimeonTrenev@users.noreply.github.com>
2021-06-15 18:18:27 +0300

    add teams

--- src/components/AddTeams.js
+++ src/components/AddTeams.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React from "react";
+
 
 class AddTeams extends React.Component {
-    constructor(props){
-        super(props)
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div>
-                Add Teams
-            </div>
-        )
-    }
+  // onHandleChange = (e) => {
+  //     e.preventDefault();
+  //     this.setState({ [e.target.name] : e.target.value })
+
+  // }
+
+  // onClickChange = (e) => {
+  //     console.log('123')
+  // }
+
+  // onSubmitInput = (e) => {
+  //     console.log('The input was submitted')
+  // }
+
+  render() {
+    const { type, label, name, value, onChange } = this.props;
+
+    return (
+      <div>
+        <div className="ui input">
+          <label>{label}</label>
+          <input
+            type={type}
+            placeholder={label} 
+            value={value}
+            onChange={onChange}
+            name={name}
+          ></input>
+        </div>
+          
+      </div>
+    );
+  }
 }
 
 export default AddTeams;
--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,15 +1,61 @@
 import React from 'react';
+import AddTeams from './AddTeams'
+
 
 class Teams extends React.Component {
     constructor(props) {
       super(props)  
+      this.state = {
+          country: '',
+          teamHistory: '',
+          numberOfPlayers: ''
+      }
+    }
+
+    buttonClicked = (e) => {
+        e.preventDefault();
+        console.log(this.state)
+        this.setState({
+            country: '',
+            teamHistory: '',
+            numberOfPlayers: ''
+        })
+    }
+
+    onHandleChange = (e) => {
+        e.preventDefault();
+        this.setState({ [e.target.name] : e.target.value })
+     
     }
 
     render() {
         return(
             <div className="ui container">
-                <h4>Teams</h4>
-                <h6>{this.props.teams}</h6>
+                <form>
+                <AddTeams 
+                type="text" 
+                label="Държава"
+                name="country"
+                value={this.state.country}
+                onChange = {this.onHandleChange}
+                />
+                <AddTeams 
+                type="text" 
+                label="История"
+                name="teamHistory"
+                value={this.state.teamHistory}
+                onChange = {this.onHandleChange}
+                />
+                <AddTeams 
+                type="text" 
+                label="Брой играчи"
+                name="numberOfPlayers"
+                value={this.state.numberOfPlayers}
+                onChange = {this.onHandleChange}
+                />
+
+                </form>
+                <button onClick={this.buttonClicked} className="ui primary button">Добави</button>
             </div>
         )
     }
