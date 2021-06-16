a7ab4a19d83af3375e5a0317d43a48b9b0b2e08e
Simeon Trenev <65025840+SimeonTrenev@users.noreply.github.com>
2021-06-16 12:26:25 +0300

    commit

--- src/components/Tables.js
+++ src/components/Tables.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 class Tables extends React.Component {
+    constructor(props){
+        super(props)
+        
+    }
+    
     render() {
+        const { value1, value2, value3 } = this.props;
         return(
             <div>
-                Класиране
+                <form>
+                    <input value={value1} />
+                    <input value={value2} />
+                    <input value={value3} />
+                </form>
             </div>
         )
     }
--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,64 +1,69 @@
-import React from 'react';
-import AddTeams from './AddTeams'
-
+import React from "react";
+import AddTeams from "./AddTeams";
+import Tables from "./Tables";
 
 class Teams extends React.Component {
-    constructor(props) {
-      super(props)  
-      this.state = {
-          country: '',
-          teamHistory: '',
-          numberOfPlayers: ''
-      }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      country: "",
+      teamHistory: "",
+      numberOfPlayers: "",
+    };
+  }
 
-    buttonClicked = (e) => {
-        e.preventDefault();
-        console.log(this.state)
-        this.setState({
-            country: '',
-            teamHistory: '',
-            numberOfPlayers: ''
-        })
-    }
+  buttonClicked = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem("country", this.state.country);
+    sessionStorage.setItem("teamHistory", this.state.teamHistory);
+    sessionStorage.setItem("numberOfPlayers", this.state.numberOfPlayers);
 
-    onHandleChange = (e) => {
-        e.preventDefault();
-        this.setState({ [e.target.name] : e.target.value })
-     
-    }
+    this.setState({
+      country: "",
+      teamHistory: "",
+      numberOfPlayers: "",
+    });
+  };
 
-    render() {
-        return(
-            <div className="ui container">
-                <form>
-                <AddTeams 
-                type="text" 
-                label="Държава"
-                name="country"
-                value={this.state.country}
-                onChange = {this.onHandleChange}
-                />
-                <AddTeams 
-                type="text" 
-                label="История"
-                name="teamHistory"
-                value={this.state.teamHistory}
-                onChange = {this.onHandleChange}
-                />
-                <AddTeams 
-                type="text" 
-                label="Брой играчи"
-                name="numberOfPlayers"
-                value={this.state.numberOfPlayers}
-                onChange = {this.onHandleChange}
-                />
+  onHandleChange = (e) => {
+    e.preventDefault();
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-                </form>
-                <button onClick={this.buttonClicked} className="ui primary button">Добави</button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="ui container">
+        <form>
+          <AddTeams
+            type="text"
+            label="Държава"
+            name="country"
+            value={this.state.country}
+            onChange={this.onHandleChange}
+          />
+          <AddTeams
+            type="text"
+            label="История"
+            name="teamHistory"
+            value={this.state.teamHistory}
+            onChange={this.onHandleChange}
+          />
+          <AddTeams
+            type="text"
+            label="Брой играчи"
+            name="numberOfPlayers"
+            value={this.state.numberOfPlayers}
+            onChange={this.onHandleChange}
+          />
+        </form>
+        <button onClick={this.buttonClicked} className="ui primary button">
+          Добави
+        </button>
+        <Tables value1={sessionStorage.getItem("country")} value2 = {sessionStorage.getItem("teamHistory")} value3 = {sessionStorage.getItem("numberOfPlayers")} />
+    
+      </div>
+    );
+  }
 }
 
 export default Teams;
